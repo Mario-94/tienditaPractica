19ccdd760b665238bc7ef60a2f871d89d45944a2
Mario-PC <mags9415@gmail.com>
2024-02-02 15:51:33 -0600

    NavBar base

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../styles/navBar.css";
+
 const NavBar = () => {
   return (
     <>
@@ -12,15 +13,15 @@ const NavBar = () => {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
+            data-bs-target="#collapse"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse  " id="collapse">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 alignText ">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to={"/"}>
                   <h6>Inicio</h6>
@@ -40,7 +41,7 @@ const NavBar = () => {
                 aria-label="Search"
               />
               <input
-                className="btn btn-outline-success d-flex"
+                className="btn  btn-outline-light containerButton "
                 type="submit"
                 value={"Search"}
               />
