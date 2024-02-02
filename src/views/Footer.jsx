5f827eaae492692a1cd93cfa5d52dbba0e3f8889
Mario-PC <mags9415@gmail.com>
2024-02-02 14:04:55 -0600

    base de carrusel

---
+++ src/views/Footer.jsx
@@ -0,0 +1,14 @@
+import "../styles/footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer bg-light">
+      <div className="container text-center">
+        <span className="text-muted">© 2024 Tu Empresa</span>
+        <span className="text-muted">© 2024 Tu Empresa</span>
+        <p className="text-muted">© 2024 Tu Empresa</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
