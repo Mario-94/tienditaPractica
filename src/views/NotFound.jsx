d4580568ad0b720adb7037d10b57ce4686b80be2
Mario-PC <mags9415@gmail.com>
2024-02-03 13:58:35 -0600

    Base de componente Productos

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="container-fluid text-center">
+      <h1>NotFound</h1>
+    </div>
+  );
 };
 
 export default NotFound;
